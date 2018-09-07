29413ffff5dfaa74a5a11f8b3fb646a97507b51d
Nicholas <nickmx1896@Hotmail.com>
2018-09-08 01:01:15 +0800

    added notification alert and refresh button

--- app/src/main/java/com/example/nickm/fypapplication/MainActivity.java
+++ app/src/main/java/com/example/nickm/fypapplication/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.nickm.fypapplication;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -18,16 +24,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        String little = "LITTLE Governor: " + getGovernorLittle();
+        String big = "big Governor: " + getGovernorBig();
 
+        TextView textView1 = (TextView)findViewById(R.id.textView1);
+        TextView textView2 = (TextView)findViewById(R.id.textView2);
         TextView textView3 = (TextView)findViewById(R.id.textView3);
-        TextView textView6 = (TextView)findViewById(R.id.textView6);
-        textView3.setText("LITTLE Governor: " + getGovernorLittle());
-        textView6.setText("big Governor: " + getGovernorBig());
+        textView1.setText(little);
+        textView2.setText(big);
+        textView3.setText(getScheduler());
+
+
+    }
+
+    public void showNotification(View v)
+    {
+        //PendingIntent update = PendingIntent.getActivity(this,0,update,PendingIntent.FLAG_ONE_SHOT);
+        // helps sets certain parameters of the notification, like icons etc
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setSmallIcon(R.drawable.ic_launcher)
+                .setContentTitle("CPU Details");
+        builder.setStyle(new NotificationCompat.InboxStyle()    // allows the notification to become bigger
+                .addLine("big Governor:   " + getGovernorBig())
+                .addLine("LITTLE Governor:   " + getGovernorLittle()));
+        //builder.addAction(R.mipmap.ic_launcher, "refresh",update);
+        NotificationManager NM = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        NM.notify(0,builder.build());
 
     }
 
 
-    private String getGovernorLittle() {
+    public void refresh (View v){
+        finish();
+        startActivity(getIntent());
+    }
+
+    private String getGovernorBig() {
         StringBuffer sb = new StringBuffer();
 
         //String file = "/proc/cpuinfo";  // Gets most cpu info (but not the governor)
@@ -50,11 +82,34 @@ public class MainActivity extends AppCompatActivity {
 
         return sb.toString();
     }
-    private String getGovernorBig() {
+    private String getGovernorLittle() {
         StringBuffer sb = new StringBuffer();
 
         //String file = "/proc/cpuinfo";  // Gets most cpu info (but not the governor)
         String file = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor";  // Gets governor for LITTLE cores
+
+        if (new File(file).exists()) {
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(new File(file)));
+                String aLine;
+                while ((aLine = br.readLine()) != null)
+                    sb.append(aLine + "\n");
+
+                if (br != null)
+                    br.close();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return sb.toString();
+    }
+    private String getScheduler() {
+        StringBuffer sb = new StringBuffer();
+
+        //String file = "/proc/cpuinfo";  // Gets most cpu info (but not the governor)
+        String file = "/sys/block/sda/queue/scheduler";  // Gets governor for big cores
 
         if (new File(file).exists()) {
             try {
